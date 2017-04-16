10c5b7849b710d6e8121f886e08e6679de21448a
hsndev <hsndev@hotmail.com>
2017-04-16 09:57:05 -0400

    Fist change - add two msgs

--- ProjetGitEclipse/src/AppCtr.java
+++ ProjetGitEclipse/src/AppCtr.java
@@ -3,5 +3,7 @@ public class AppCtr {
 	public static void main (String[] args){
 		System.out.println("hello worl git!");
 		//
+		System.out.println("Second msg");
+		System.out.println("Second msg");
 	}
 }
